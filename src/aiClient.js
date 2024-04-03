726ff72639976294396daf484ce9b90e6ba1349a
ichigo_kurosaki <125087781+ichigo-k@users.noreply.github.com>
2024-04-03 12:37:06 +0000

    Made a bot on twitter that automatically sends morning greetings

---
+++ src/aiClient.js
@@ -0,0 +1,22 @@
+import { GoogleGenerativeAI } from "@google/generative-ai"
+const client = new GoogleGenerativeAI(process.env.AI_API_KEY)
+
+
+const prompt = async ()=>{
+    try {
+      
+      const model = client.getGenerativeModel({model:"gemini-pro"})
+      const prompt = "Hello write a good morning message for me. it is going to be posted on twitter. the quote should be about programming and should adddress devs please you can also add some emojis where needed. also in english and needed tags to make it viral. Also it should not be too long it should meet twitter required lenght"
+
+      const result = await model.generateContent(prompt)
+      const response = result.response
+      const text = response.text()
+
+      return text
+
+    } catch (error) {
+      console.log(error)
+    }
+}
+
+export default prompt
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import "dotenv/config"
+import express from "express"
+import  exports from "./twitterClient.js"
+import { CronJob } from "cron"
+import prompt from "./aiClient.js"
+
+const app = express()
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Server is running")
+})
+
+const tweet = async(message)=>{
+    try {
+        await exports.twitterClient.v2.tweet(message)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+
+const cronTweet = new CronJob("* * * * *", async () => {
+    try {
+        tweet( await prompt())
+        console.log("sent ...")
+    } catch (error) {
+        console.log(error)
+    }
+  });
+  
+  cronTweet.start();
+
+
+
+
+
+  
